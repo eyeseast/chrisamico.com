3a68eec059c30b22f1d204ad32b1c95bdad85745
Chris Amico <eyeseast@gmail.com>
2017-01-15 15:45:13 -0500

    Basic link loader

---
+++ load_links.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+"""
+Useful things
+"""
+import datetime
+import time
+
+import dataset
+import feedparser
+import requests
+
+from opengraph import OpenGraph
+from sqlalchemy import types
+from tarbell_config import FEEDS, DATABASE_URL, LINK_TABLE
+
+
+class LinkLoader(object):
+    """
+    Loop through FEEDS
+    Fetch open graph data for each link
+    Save OG for each (plus some other metadata) to database
+    """
+    TYPES = {
+        'latitude': types.Float,
+        'longitude': types.Float,
+    }
+
+    def __init__(self, database_url, table_name, feeds):
+        self.db = dataset.connect(database_url)
+        self.table = self.db[table_name]
+        self.feeds = feeds
+
+    def run(self):
+        "Do the whole download"
+        for name, url in self.feeds:
+            for link in self.handle_feed(name, url):
+                if self.table.upsert(link, ['url'], types=self.TYPES):
+                    print(link['url'])
+
+    def handle_feed(self, name, url):
+        "Parse feed URL, yield links reading for the database"
+        r = requests.get(url)
+        feed = feedparser.parse(r.content)
+
+        for entry in feed.entries:
+            # bail here if we already have this URL
+            if self.table.find_one(_url=entry.link):
+                continue
+
+            if 'published_parsed' in entry:
+                date = datetime.datetime.fromtimestamp(time.mktime(entry.published_parsed))
+            else:
+                date = datetime.datetime.now()
+
+            og = self.handle_link(entry.link, 
+                url=entry.link,
+                date=date,
+                feed=name)
+
+            if og:
+                yield og
+
+    def handle_link(self, link, **defaults):
+        "Fetch OG data and return a dict ready for the db"
+        r = requests.get(link)
+        if r.ok:
+            og = OpenGraph(html=r.content)
+            defaults.update(og)
+            defaults['_url'] = link
+
+            return defaults
+
+
+def main():
+    """
+    Run a LinkLoader
+    """
+    LinkLoader(DATABASE_URL, LINK_TABLE, FEEDS).run()
+
+
+if __name__ == '__main__':
+    main()
--- tarbell_config.py
+++ tarbell_config.py
@@ -2,16 +2,29 @@
 """
 site settings
 """
+import os
+import sys
+
+PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
+sys.path.append(PROJECT_ROOT)
 
 from flask import Blueprint, g
 
-blueprint = Blueprint('blog', __name__)
+DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///blog.db')
+LINK_TABLE = "links"
+
+FEEDS = (
+    ('instapaper', 'https://www.instapaper.com/starred/rss/13475/qUh7yaOUGOSQeANThMyxXdYnho'),
+    ('newsblur', 'https://chrisamico.newsblur.com/social/rss/35501/chrisamico'),
+)
 
 OG = {
     'title': 'This is my website',
     'type': 'website',
 }
 
+
+blueprint = Blueprint('blog', __name__)
 
 @blueprint.app_context_processor
 def add_to_context():
