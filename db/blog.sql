455eee62f85e5826fd6b63d075df43c186d2a649
Automated <actions@users.noreply.github.com>
2024-02-21 18:10:47 +0000

    Updated: Wed Feb 21 18:10:47 UTC 2024

--- db/blog.sql
+++ db/blog.sql
@@ -9,7 +9,7 @@ CREATE TABLE [feeds] (
    [updated] TEXT
 );
 INSERT INTO feeds VALUES('http://chrisamico.newsblur.com/','chrisamico''s blurblog',NULL,'http://chrisamico.newsblur.com/','chrisamico','2021-07-06T14:30:43Z');
-INSERT INTO feeds VALUES('https://www.instapaper.com/liked','Instapaper: Starred','','https://www.instapaper.com/liked',NULL,'2024-02-21T18:00:45.109724');
+INSERT INTO feeds VALUES('https://www.instapaper.com/liked','Instapaper: Starred','','https://www.instapaper.com/liked',NULL,'2024-02-21T18:10:41.927238');
 INSERT INTO feeds VALUES('https://chrisamico.newsblur.com/','chrisamico''s blurblog',NULL,'https://chrisamico.newsblur.com/','chrisamico','2024-02-21T16:26:24.067000Z');
 CREATE TABLE [links] (
    [id] TEXT PRIMARY KEY,
@@ -745,6 +745,10 @@ INSERT INTO mastodon VALUES('https://talkingpointsmemo.com/edblog/the-messenger-
 INSERT INTO mastodon VALUES('https://xkcd.com/2889/',NULL,'failed',NULL,'''Response'' object is not subscriptable');
 INSERT INTO mastodon VALUES('https://www.grapplearts.com/this-black-belts-advice-would-have-killed-me/',NULL,'failed',NULL,'''Response'' object is not subscriptable');
 INSERT INTO mastodon VALUES('https://www.grapplearts.com/this-black-belts-advice-would-have-killed-me/',NULL,'failed',NULL,'''Response'' object is not subscriptable');
+INSERT INTO mastodon VALUES('https://www.washingtonpost.com/opinions/2024/02/21/parents-children-how-to-reduce-paperwork/',NULL,'failed',NULL,'''Response'' object is not subscriptable');
+INSERT INTO mastodon VALUES('https://www.washingtonpost.com/opinions/2024/02/21/parents-children-how-to-reduce-paperwork/',NULL,'failed',NULL,'''Response'' object is not subscriptable');
+INSERT INTO mastodon VALUES('https://www.washingtonpost.com/opinions/2024/02/21/parents-children-how-to-reduce-paperwork/',NULL,'failed',NULL,'''Response'' object is not subscriptable');
+INSERT INTO mastodon VALUES('https://www.washingtonpost.com/opinions/2024/02/21/parents-children-how-to-reduce-paperwork/',NULL,'failed',NULL,'''Response'' object is not subscriptable');
 INSERT INTO mastodon VALUES('https://www.washingtonpost.com/opinions/2024/02/21/parents-children-how-to-reduce-paperwork/',NULL,'failed',NULL,'''Response'' object is not subscriptable');
 INSERT INTO mastodon VALUES('https://www.washingtonpost.com/opinions/2024/02/21/parents-children-how-to-reduce-paperwork/',NULL,'failed',NULL,'''Response'' object is not subscriptable');
 INSERT INTO mastodon VALUES('https://www.washingtonpost.com/opinions/2024/02/21/parents-children-how-to-reduce-paperwork/',NULL,'failed',NULL,'''Response'' object is not subscriptable');
