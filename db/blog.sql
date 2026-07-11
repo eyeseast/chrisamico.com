214a7717a856dd13095d5e231fbd15c24efbb9a7
Automated <actions@users.noreply.github.com>
2026-07-11 03:32:43 +0000

    Updated: Sat Jul 11 03:32:43 UTC 2026

--- db/blog.sql
+++ db/blog.sql
@@ -9,7 +9,7 @@ CREATE TABLE [feeds] (
    [updated] TEXT
 );
 INSERT INTO feeds VALUES('http://chrisamico.newsblur.com/','chrisamico''s blurblog',NULL,'http://chrisamico.newsblur.com/','chrisamico','2021-07-06T14:30:43Z');
-INSERT INTO feeds VALUES('https://www.instapaper.com/liked','Instapaper: Starred','','https://www.instapaper.com/liked',NULL,'2026-07-10T19:46:13.766868');
+INSERT INTO feeds VALUES('https://www.instapaper.com/liked','Instapaper: Starred','','https://www.instapaper.com/liked',NULL,'2026-07-11T03:32:34.408780');
 INSERT INTO feeds VALUES('https://chrisamico.newsblur.com/','chrisamico''s blurblog',NULL,'https://chrisamico.newsblur.com/','chrisamico','2026-07-10T02:58:59.348000Z');
 CREATE TABLE [links] (
    [id] TEXT PRIMARY KEY,
